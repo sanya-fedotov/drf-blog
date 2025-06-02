f44fb3e606aca883e9b282df26e0593b6f83ff3a
Aleksandr Fedotov <sashafusa@mail.ru>
2025-06-02 14:04:59 +0300

    Feat: Create models Post and Tag

--- drf/blog/models.py
+++ drf/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)    
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
